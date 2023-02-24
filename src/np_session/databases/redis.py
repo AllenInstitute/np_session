97a8ddc351b78b702bc90aab60403281bb48220c
bjhardcastle <ben.hardcastle@alleninstitute.org>
2023-02-23 22:54:11 -0800

    Show dict items

--- src/np_session/databases/redis.py
+++ src/np_session/databases/redis.py
@@ -63,7 +63,7 @@ class State(collections.abc.MutableMapping):
             self.__class__.connect()
 
     def __repr__(self) -> str:
-        return f"{self.__class__.__name__}({self.name!r})"
+        return self.data.__repr__()
     
     @classmethod
     def connect(cls) -> None:
