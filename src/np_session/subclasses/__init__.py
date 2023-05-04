efb2f3b7ab6dedb46c7577ed9f3cafb93a630ab9
bjhardcastle <ben.hardcastle@alleninstitute.org>
2023-05-04 12:22:43 -0700

    Add Templeton to subclasses

--- src/np_session/subclasses/__init__.py
+++ src/np_session/subclasses/__init__.py
@@ -1,4 +1,5 @@
 from np_session.subclasses.dynamic_routing_pilot import DRPilotSession
 from np_session.subclasses.pipeline import PipelineSession
+from np_session.subclasses.templeton_pilot import TempletonPilotSession
 
-__all__ = ['DRPilotSession', 'PipelineSession']
+__all__ = ['DRPilotSession', 'PipelineSession', 'TempletonPilotSession']
